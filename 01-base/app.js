94c006c9b5fa4b46ea7563972ec255e6fa099b5e
FlerkoChulpan <sattarova.chul@gmail.com>
2023-07-18 19:39:06 +0300

    Начало работы

---
+++ 01-base/app.js
@@ -0,0 +1,21 @@
+let num = 42; 
+let firstName = `Chulpan`;
+const isProgrammer = true;
+
+/* CAN DO
+$
+$num
+num$
+_
+_num
+num_
+myNum
+num1
+*/
+
+/* RESTRICTED
+42num
+my-num
+let, var, const, true, false, alert
+*/
+
