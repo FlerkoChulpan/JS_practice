9605bf2c990800d13f96c2b59da923a9f6120085
FlerkoChulpan <sattarova.chul@gmail.com>
2023-07-20 09:12:13 +0300

    Работа с калькулятором, знакомство с операторами

--- 01-base/app.js
+++ 01-base/app.js
@@ -1,21 +1,84 @@
-let num = 42; 
-let firstName = `Chulpan`;
-const isProgrammer = true;
+// let num = 42; 
+// let firstName = `Chulpan`;
+// const isProgrammer = true;
 
-/* CAN DO
-$
-$num
-num$
-_
-_num
-num_
-myNum
-num1
-*/
+// /* CAN DO
+// $
+// $num
+// num$
+// _
+// _num
+// num_
+// myNum
+// num1
+// */
 
-/* RESTRICTED
-42num
-my-num
-let, var, const, true, false, alert
-*/
+// /* RESTRICTED
+// 42num
+// my-num
+// let, var, const, true, false, alert
+// */
+
+// console.log(num+10);
+// console.log(num-10);
+// console.log(num*10);
+// console.log(num/10);
+
+// let num2=num+10;
+// console.log(num, num2);
+
+// num=num2-num;
+// console.log(num,num2);
+
+const resultElement = document.getElementById('result');
+const input1=document.getElementById('input1');
+const input2=document.getElementById('input2');
+const submitBtn=document.getElementById('submit');
+const plusBtn=document.getElementById('plus');
+const minusBtn=document.getElementById('minus');
+let action = '+';
+
+plusBtn.onclick=function(){
+    action='+';
+}
+
+minusBtn.onclick=function(){
+    action='-';
+}
+function printResult(result){
+    if (result<0){
+        resultElement.style.color= 'red';
+    }
+    else{
+        resultElement.style.color='green';
+    }
+    resultElement.textContent=result;
+}
+
+function computeNumbersWithAction(input1, input2,actionSymbol){
+    const num1=Number(inp1.value);
+    const num2=Number(inp2.value);
+    // if (actionSymbol=='+'){
+    //     return num1+num2;
+    // } else if (actionSymbol=='-'){
+    //     return num1-num2;
+    // }
+
+    return actionSymbol=='+' ? num1+num2 : num1-num2;
+}
+
+submitBtn.onclick = function(){
+    const result = computeNumbersWithAction(input1, input2, action);
+    printResult(result);
+
+    // if(action=='+'){
+    //     const sum = Number(input1.value) + Number(input2.value);
+    //     printResult(sum);
+    // }
+    // else {
+    //     const sum = Number(input1.value) - Number(input2.value);
+    //     printResult(sum);
+    // }
+}
+
 
---
+++ 01-base/theory.js
@@ -0,0 +1,29 @@
+//==================Operators
+// let a = 10;
+// let b=5;
+// let c=32;
+
+// c= c+a;
+// c +=a;
+
+// console.log(c);
+
+//==================Data Types
+// const age = 23;
+// const name='Chulpan';
+// const isProgrammer=true;
+// let x;
+// console.log(typeof )
+
+
+// =================Priority
+// const fullAge=23;
+// const birthYear =1999;
+// const currentYear=2023;
+// const isFullAge = currentYear-birthYear >= fullAge;
+// console.log(isFullAge)
+
+// const num1=42;
+// const num2='42';
+
+// console.log(num1===num2)
