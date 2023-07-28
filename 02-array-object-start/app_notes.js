97a74ad95331c6e34609b0634dd26305b92e28af
FlerkoChulpan <sattarova.chul@gmail.com>
2023-07-28 08:22:50 +0300

    Доделан блокнот с заметками

--- 02-array-object-start/app_notes.js
+++ 02-array-object-start/app_notes.js
@@ -4,49 +4,39 @@ const listElement=document.getElementById('list');
 const notes = [
     {
         title: 'Название заметки',
-        completed: false, //выполнено ли задание 
+        completed: false, //невыполнено задание 
     },
     {
         title: 'Название заметки 2',
-        completed: true, //выполнено ли задание 
+        completed: true, //выполнено задание 
     }
 ]
 // console.log(inputElement.value);
 
 // const notes=['notes1', 'notes2'];
 
-function getNoteTemplate (note) {
-    console.log(note.completed)
-    return `
-    <li
-                class="list-group-item d-flex justify-content-between align-items-center"
-                >
-                <span class="${note.completed ? 'text-decoration-line-through' :''}">${note.title}</span>
-                <span>
-                    <span class="btn btn-small btn-success">&check;</span>
-                    <span class="btn btn-small btn-danger">&times;</span>
-                </span>
-            </li>
-    `
-}
 
 function render () {
-    // for (let i=0; i < notes.length; i++) {
-    //     listElement.insertAdjacentHTML(
-    //         'beforeend', 
-    //         getNoteTemplate(notes[i])
-    //     )
-    // }
-
-    for (let note of notes) {
+    listElement.innerHTML='';
+    if (notes.length ===0){
+        listElement.innerHTML='<p>Нет заметок</p>'
+    }
+    for (let i=0; i < notes.length; i++) {
         listElement.insertAdjacentHTML(
             'beforeend', 
-            getNoteTemplate(note)
+            getNoteTemplate(notes[i], i)
         )
     }
+
+    // for (let note of notes) {
+    //     listElement.insertAdjacentHTML(
+    //         'beforeend', 
+    //         getNoteTemplate(note)
+    //     )
+    // }
 }
 
-render
+render()
 
 createBtn.onclick = function () {
     if(inputElement.value.length ===0) {
@@ -57,10 +47,46 @@ createBtn.onclick = function () {
         completed: false,
     }
 
-    listElement.insertAdjacentHTML(
-        'beforeend', 
-        getNoteTemplate(newNote)
-    )
+    // listElement.insertAdjacentHTML(
+    //     'beforeend', 
+    //     getNoteTemplate(newNote)
+    // )
+    notes.push(newNote);
+    render();
+
     inputElement.value=''
 }
 
+listElement.onclick=function (event) {
+    if (event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index);
+        const type = event.target.dataset.type;
+
+        if (type === 'toggle'){
+            notes[index].completed = !notes[index].completed
+            //console.log('toggle', index);
+        }
+            else if (type === 'remove'){
+                notes.splice(index, 1)
+                //console.log('remove', index);
+            }
+
+        render()
+    }
+}
+
+function getNoteTemplate (note, index) {
+    return `
+    <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+    >
+        <span class="${note.completed ? 'text-decoration-line-through' :''}">${note.title}</span>
+        <span>
+        <span class="btn btn-small btn-${
+            note.completed ? 'warning' : 'success'
+        }" data-index="${index}" data-type="toggle">&check;</span>
+        <span class="btn btn-small btn-danger"  data-type="remove" data-index="${index}" >&times;</span>
+        </span>
+    </li>
+    `
+}
